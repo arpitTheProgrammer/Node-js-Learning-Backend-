f0e04ab431d667a5a7a0c3cf80399df7f171b6cb
arpitTheProgrammer <arpitsrivastava8863.9h@gmail.com>
2025-01-17 18:22:51 +0530

    Middleware

--- Rest API/middleware.js
+++ Rest API/middleware.js
@@ -4,3 +4,31 @@ const mw = (req, res, next) => {
     // next()
 }
 export default mw();
+
+
+
+
+
+
+//These middleware used in index.js
+
+/*
+app.use((req, res, next) => {
+    console.log("Hello From Middleware")
+    req.myUserName = "Arpit Srivastava"
+    fs.appendFile('log.txt', `${Date.now()}, ${req.method}, ${req.ip}, ${req.path}\n`, (err, data)=>{
+        next()
+    }) // Create files
+    // console.log(mw)
+    // return res.json({msg: "Hello From middleware"})
+    // next(); // Transfer to the Next middleware
+})
+
+// app.use((req, res, next) => {
+//     console.log("Hay", req.myUserName)
+//     console.log('Time', Date.now())
+//     next()
+//     // return res.end("Hay", req.myUserName)
+// })
+*/
+
